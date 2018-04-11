a192a349d9647ce85b4ebdc945b448137c356d4b
Alfredo <33386899+Negraes@users.noreply.github.com>
2018-04-11 16:41:45 -0300

    Último Commit

---
+++ Recortando.ps1
@@ -0,0 +1,21 @@
+﻿# Recortando Conteúdo
+
+
+    # Variáveis
+        $origem = "C:\origem"
+        $destino = "C:\destino"
+
+
+    # Copiar
+        # Copiar Todos os Sub-Diretórios Sobrescrevendo
+        Copy-Item -Path $origem\* -Destination $destino -Recurse -Force 
+
+        # Copiar todos os Arquivos Sobrescrevendo
+        Copy-Item -Path $origem\*.* -Destination $destino -Force 
+
+    # Apagar
+        # Apaga todo os arquivos da Pasta
+        Remove-Item $origem\* -Recurse -Force 
+
+        # Apaga todo os arquivos da Pasta
+        Remove-Item $origem\*.* -Force 
